f5429ee27fe25e47b7c11eaebf4f4551de88989d
Denis Gandzha <gandja.denis@yandex.ru>
2021-01-20 17:39:55 +0300

    Current version for people-directory service

---
+++ backend/people-directory-service/src/main/java/com/luxoft/gandzha/peopledirectoryservice/PeopleDirectoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.gandzha.peopledirectoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleDirectoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleDirectoryServiceApplication.class, args);
+	}
+
+}
---
+++ backend/people-directory-service/src/test/java/com/luxoft/gandzha/peopledirectoryservice/PeopleDirectoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.gandzha.peopledirectoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleDirectoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
